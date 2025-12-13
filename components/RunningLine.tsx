d73b3d2cbe7fcc9403a619e08ebec01683695145
Artem <artqbegame@gmail.com>
2025-12-13 03:21:18 +0300

    feat(component): add enhanced control options
    - Added support for reverse scrolling direction
    - Introduced isPausedOnHover prop to pause scrolling on mouse hover
    - Introduced isPausedOnClick prop to pause scrolling on click

--- components/RunningLine.tsx
+++ components/RunningLine.tsx
@@ -7,9 +7,18 @@ import { useRef, useState } from "react";
 interface RunningLineProps {
   children: React.ReactNode;
   speed?: number; // px per second
+  isReversed?: boolean; 
+  isPausedOnHover?: boolean;
+  isPausedOnClick?: boolean;
 }
 
-function RunningLine({ children, speed = 35 }: RunningLineProps) {
+function RunningLine({
+  children,
+  speed = 35,
+  isReversed = false,
+  isPausedOnHover = true,
+  isPausedOnClick = false,
+}: RunningLineProps) {
   const [animationState, setAnimationState] = useState<
     "start" | "pause" | "resume"
   >("start");
@@ -21,12 +30,16 @@ function RunningLine({ children, speed = 35 }: RunningLineProps) {
   const [contentWidth, setContentWidth] = useState(0);
   const [currentX, setCurrentX] = useState(0);
 
-  const startX = -contentWidth;
-  const finishX = containerWidth;
+  const [isAnimationEnabled, setIsAnimationEnabled] = useState(true);
+
+  const [startX, finishX] = isReversed
+    ? [-contentWidth, containerWidth]
+    : [containerWidth, -contentWidth];
+
   const totalDistance = containerWidth + contentWidth;
   const absoluteDuration = totalDistance > 0 ? totalDistance / speed : 0;
 
-  const handleMouseEnter = () => {
+  const pauseAnimation = () => {
     if (contentRef.current && containerRef.current) {
       const rectContent = contentRef.current.getBoundingClientRect();
       const rectContainer = containerRef.current.getBoundingClientRect();
@@ -35,17 +48,38 @@ function RunningLine({ children, speed = 35 }: RunningLineProps) {
     }
   };
 
-  const handleMouseLeave = () => {
-    const remainingDistance = finishX - currentX;
+  const resumeAnimation = () => {
+    const remainingDistance = Math.abs(finishX - currentX);
     const newDuration = remainingDistance > 0 ? remainingDistance / speed : 0;
     setDuration(newDuration);
     setAnimationState("resume");
   };
 
+  const handleClick = () => {
+    if (isPausedOnClick) {
+      if (isPausedOnHover) {
+        setIsAnimationEnabled(!isAnimationEnabled);
+      } else {
+        animationState === "pause" ? resumeAnimation() : pauseAnimation();
+      }
+    }
+  };
+
+  const handleMouseEnter = () => {
+    if (isAnimationEnabled && isPausedOnHover) {
+      pauseAnimation();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (isAnimationEnabled && isPausedOnHover) {
+      resumeAnimation();
+    }
+  };
+
   const handleContainerWidthChange = (newWidth: number) => {
     setContainerWidth(newWidth);
   };
-
   const handleContentWidthChange = (newWidth: number) => {
     setContentWidth(newWidth);
   };
@@ -87,8 +121,8 @@ function RunningLine({ children, speed = 35 }: RunningLineProps) {
       ref={containerRef}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
       className="overflow-hidden whitespace-nowrap"
-      aria-hidden="true"
     >
       <motion.div
         ref={contentRef}
